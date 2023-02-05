c6dc14d5bc1c0c60b0b2e1297b8b5092ed9be7bc
Sushij <sushant.wurk@gmail.com>
2023-02-05 16:11:55 +0530

    day-2 init

---
+++ day-2/pokedex/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data: () => ({
+    pokedex: [1, 2, 3]
+  }),
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100").then((res) => res.json())
+    }
+  }
+}
+</script>
+
+<template>
+  <pre>{{ pokedex }}</pre>
+  <button @click="fetchPokemon">Fetch</button>
+</template>
+
